1849016e97e57f623855d3d75a1255346938acb5
Xinran Cao <xinrancao@hotmail.com>
2022-04-17 16:57:55 +0800

    comments

--- src/DP/DynamicProgramming.java
+++ src/DP/DynamicProgramming.java
@@ -2,6 +2,11 @@ package DP;
 
 public class DynamicProgramming {
 
+    /**
+     * 打印2维数组
+     * 
+     * @param dp
+     */
     public static void printDP(int[][] dp) {
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
@@ -11,6 +16,11 @@ public class DynamicProgramming {
         }
     }
 
+    /**
+     * 打印3维数组
+     * 
+     * @param dp
+     */
     public static void printDP(int[][][] dp) {
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < dp[0].length; j++) {
@@ -23,6 +33,16 @@ public class DynamicProgramming {
         }
     }
 
+    /**
+     * 检查某个位置是否越界(3维数组)
+     * 如果越界，返回0，否则返回数组中这个位置记录的值
+     * 
+     * @param dp    // 3维数组
+     * @param currH // 某个位置的高
+     * @param currX // 某个位置的长
+     * @param currY // 某个位置的宽
+     * @return
+     */
     public static int checkInside(int[][][] dp, int currH, int currX, int currY) {
         if (currH < 0 || currX < 0 || currY < 0 || currH > dp.length - 1
                 || currX > dp[0].length - 1 || currY > dp[0][0].length - 1) {
@@ -31,6 +51,15 @@ public class DynamicProgramming {
         return dp[currH][currX][currY];
     }
 
+    /**
+     * 检查某个位置是否越界(2维数组)
+     * * 如果越界，返回0，否则返回数组中这个位置记录的值
+     * 
+     * @param dp    // 2维数组
+     * @param currX // 某个位置的长
+     * @param currY // 某个位置的宽
+     * @return
+     */
     public static int checkInside(int[][] dp, int currX, int currY) {
         if (currX < 0 || currY < 0
                 || currX > dp.length - 1 || currY > dp[0].length - 1) {
@@ -39,38 +68,87 @@ public class DynamicProgramming {
         return dp[currX][currY];
     }
 
+    /**
+     * 动态规划解决币值组成价值问题
+     * 用给定币值的硬币组成一定的价值，每种币值可以任意张，有多少种组成方法
+     * 如：给定4种币值[2,3,5,7]，组成10块钱
+     * 共有5种组成办法：2+2+2+2+2，2+2+3+3，2+3+5，3+7，5+5
+     * 
+     * @param arr
+     * @param cost
+     * @return
+     */
     public static int coinsCompDP(int[] arr, int cost) {
+        // 动态规划数组
+        // 长度：从 0 到 arr数组长度，所以 length + 1
+        // 宽度：从 0 到 cost价值，所以 cost + 1
         int[][] dp = new int[arr.length + 1][cost + 1];
 
+        // 当最后一种币值考虑完毕，剩下0块钱，代表之前币值的组成方法成功得到cost价值，
+        // 动态规划数组中记录为1
+        // 其他位置，由于array初始化为0，暂时不需要填写
         dp[arr.length][0] = 1;
-        for (int j = 1; j <= cost; j++) {
-            dp[arr.length][j] = 0;
-        }
 
+        // 从底层向上填写，从左边向右边填写。
         for (int i = arr.length - 1; i >= 0; i--) {
             for (int j = 0; j <= cost; j++) {
+                // 每个位置依赖于两个位置，并且为两者记录之和：
+                // 1：正下方的记录。因为代表当前币值不参与，继续用剩下币值组合出价值
+                // 2：本币值，从0张开始，一直到N张（N*币值<=价值），每种情况下，可以成功的组合方式数
+                // 第2种是当前位置下方一行中，多个位置之和。这个和恰好是当前行、当前价值减去当前币值所在位置的记录
+                // 所以可以简化为 1 2 两种情况下，两个记录之和
                 dp[i][j] += dp[i + 1][j] + checkInside(dp, i, j - arr[i]);
             }
         }
-
+        // 从0位置的币值开始考虑，需要组成cost价值，所以这个位置的记录就是答案
         return dp[0][cost];
-
     }
 
+    /**
+     * 求最大公因数
+     * 
+     * @param m
+     * @param n
+     * @return
+     */
     public static long gcd(long m, long n) {
+        // 始终可以保持 m > n
+        // 因为：如果 m < n, m % n = m, gcd(n, m % n)等于将两者颠倒
+        // m > n 时，m % n 得到余数，相当于辗转相除
+        // 当可以除尽时，n == 0， 代表最大公因数是m
         return n == 0 ? m : gcd(n, m % n);
     }
 
+    /**
+     * 动态规划解决生存问题
+     * 给一个范围，给一个当前位置，给一个步数，每步必须向前后左右任意方向移动1的距离
+     * 如果走出范围，则生存失败，如果最终在有效范围内，则生存成功
+     * 生存成功的概率是多少
+     * 
+     * @param X     给定范围的长
+     * @param Y     给定范围的宽
+     * @param currX 当前位置的长
+     * @param currY 当前位置的宽
+     * @param steps 步数
+     * @return
+     */
     public static String surviveDP(int X, int Y, int currX, int currY, int steps) {
+        // 动态规划数组，高从 0 到 steps，所以为steps + 1
+        // 长和宽都不能超过给定范围，所以分别为X 和 Y
         int[][][] dp = new int[steps + 1][X][Y];
+        // 初始化数据，如果走完所有步数时，在有效范围内，则生存成功，成功的方法为1
         for (int i = 0; i < X; i++) {
             for (int j = 0; j < Y; j++) {
                 dp[0][i][j] = 1;
             }
         }
+        // 从step剩1开始填写
         for (int height = 1; height <= steps; height++) {
             for (int i = 0; i < X; i++) {
                 for (int j = 0; j < Y; j++) {
+                    // 每个位置依赖于4个位置，并为这些位置的记录之和
+                    // 分别代表向前后左右移动1的距离
+                    // 如果所依赖的位置越界，所依赖位置的记录为0
                     dp[height][i][j] += checkInside(dp, height - 1, i + 1, j);
                     dp[height][i][j] += checkInside(dp, height - 1, i - 1, j);
                     dp[height][i][j] += checkInside(dp, height - 1, i, j + 1);
@@ -78,20 +156,43 @@ public class DynamicProgramming {
                 }
             }
         }
-
+        // 所有的可能性
         long all = (long) Math.pow(4, steps);
+        // 存活的可能性
         long res = (long) dp[steps][currX][currY];
+        // 得到最大公因数
         long divider = gcd(all, res);
 
+        // 返回存活的可能性（分数形式）
         return String.valueOf((res / divider) + "/" + (all / divider));
     }
 
+    /**
+     * 动态规划解决象棋中马走到目标位置的问题
+     * 给一个棋盘的范围，给一个目标位置，给一个规定步数
+     * 走规定步数次后，马从原点(0,0)到达目标位置总共有多少种走法
+     * 
+     * @param X     棋盘的长
+     * @param Y     棋盘的宽
+     * @param currX 目标位置的长
+     * @param currY 目标位置的宽
+     * @param steps
+     * @return
+     */
     public static int wayChessDP(int X, int Y, int currX, int currY, int steps) {
-        int[][][] dp = new int[steps + 1][X + 1][Y + 1];
+        // 动态规划数组
+        // 从 0 到 step 步数，所以高为step + 1
+        // 不可以超出范围，所以长宽分别为X Y
+        int[][][] dp = new int[steps + 1][X][Y];
+        // 当剩0步时，如果再目标位置，则成功的方法为1
         dp[0][currX][currY] = 1;
+        // 从剩1步开始依次填写
         for (int height = 1; height <= steps; height++) {
-            for (int i = 0; i <= X; i++) {
-                for (int j = 0; j <= Y; j++) {
+            for (int i = 0; i < X; i++) {
+                for (int j = 0; j < Y; j++) {
+                    // 每个位置依赖于8个位置，分别是马的8个可以走的位置
+                    // 将这个8个位置的成功方法数相加
+                    // 如果越界，则为0
                     dp[height][i][j] += checkInside(dp, height - 1, i - 1, j - 2);
                     dp[height][i][j] += checkInside(dp, height - 1, i - 2, j - 1);
                     dp[height][i][j] += checkInside(dp, height - 1, i - 2, j + 1);
@@ -103,30 +204,55 @@ public class DynamicProgramming {
                 }
             }
         }
+        // 从(0,0)开始走step步，所以这个位置的记录就是答案
         return dp[steps][0][0];
     }
 
+    /**
+     * 动态规划解决两个人轮流拿牌的问题
+     * 手牌游戏
+     * 如：给一个数组[1,2,100,4] 两个玩家，轮流从最左或最右拿牌，直到拿完所有牌。得分是所有自己拿到牌的数值之和。
+     * 两个玩家都是绝对理智聪明的，会做出最有利于自己的选择。
+     * 返回赢家的最终得分
+     * 
+     * @param arr
+     * @return
+     */
     public static int takeCardGameDP(int[] arr) {
         int length = arr.length;
+        // 先手后手两个动态规划数组
+        // 长和宽分别表示数组中剩下牌的起始位置和终止位置
         int[][] firstDP = new int[length][length];
         int[][] secondDP = new int[length][length];
+
+        // 初始化两个数组
+        // 当剩下最后一张牌时，i == j
+        // 对于先手者，还能拿的分数就是剩余牌的价值
+        // 对于后手者，没有再加分的机会
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
                 if (i == j) {
+                    // 所以在这些位置分别记录还可以增加的分数
                     firstDP[i][j] = arr[i];
                     secondDP[i][j] = 0;
-                } else if (i > j) {
-                    firstDP[i][j] = -1;
-                    secondDP[i][j] = -1;
                 }
             }
         }
 
+        // 对角线右上角为有效位置，因为左下角i > j 代表起始位置在终止位置之后，不可能出现
         int col = 1;
+        // 沿着对角线依次填写
         while (col < length) {
             int i = 0;
             int j = col;
             while (i < length && j < length) {
+                // 对于先手者来说，有两种选择，拿开头的牌或者末尾的牌，拿完之后变成后手
+                // 所以可以得到的分数是两种选择中，可能获得更大分数的选择
+                // 所以分数等于获取到的牌的分数，以及剩下的牌中，后手情况下获得的分数
+                // 因此依赖于后手dp中，左方与下方两个位置的记录
+                // 对于后手者来说，目前得不到任何分数，但是下一轮变为先手
+                // 变为先手之前，依赖于另一个人基于对自己最好选择做出的判断
+                // 因此依赖于先手dp中左方和下方两个位置的记录，并取其较小值
                 firstDP[i][j] = Math.max(arr[i] + secondDP[i + 1][j], arr[j] + secondDP[i][j - 1]);
                 secondDP[i][j] = Math.min(firstDP[i + 1][j], firstDP[i][j - 1]);
                 i++;
@@ -134,14 +260,31 @@ public class DynamicProgramming {
             }
             col++;
         }
-
+        // 牌组从0位置到末位位置的先手和后手策略中，获得分数最大的记录就是答案
         return Math.max(firstDP[0][length - 1], secondDP[0][length - 1]);
-
     }
 
+    /**
+     * 动态规划解决拿硬币问题
+     * 给几枚硬币，最少拿多少枚硬币凑成总价值total
+     * 如：[2,3,5,7] 凑成10
+     * 有两种方法：2 + 3 + 5 或者 3 + 7
+     * 3 + 7 用了两枚硬币，最少，所以答案为2
+     * 
+     * @param coins
+     * @param total
+     * @return
+     */
     public static int wayTakeCoinDP(int[] coins, int total) {
         int length = coins.length;
+        // 动态规划数组
+        // 从第0位置的硬币开始考虑，一直到全部硬币都考虑完，所以是length + 1
+        // 从 0 到 total 价值，所以是total + 1
         int[][] dp = new int[length + 1][total + 1];
+
+        // 初始化数组，没有填过的位置未-2
+        // 无效位置为-1
+        // 如果剩余价值为0，代表成功，并且当前没再用硬币
         for (int i = 0; i <= length; i++) {
             for (int j = 0; j <= total; j++) {
                 dp[i][j] = -2;
@@ -152,58 +295,89 @@ public class DynamicProgramming {
                 }
             }
         }
-
+        // 依次填写
         for (int i = length - 1; i >= 0; i--) {
             for (int j = 1; j <= total; j++) {
+                // 每个位置依赖于两个位置，为两者之和
+                // 1：正下方的位置，代表当前硬币没拿，继续考虑之后的硬币
+                // 2：当前行，当前价值减去当前硬币价值的列
                 int p1 = dp[i + 1][j];
                 int p2 = -1;
+                // 判断是否越界，越界的话，无效
+                // 否则将其记录为p2
                 if (j - coins[i] >= 0) {
                     p2 = dp[i + 1][j - coins[i]];
                 }
+                // 判断无效
+                // 如果两者都无效，当前位置也无效
                 if (p1 == -1 && p2 == -1) {
                     dp[i][j] = -1;
-                } else if (p1 == -1) {
+                } else if (p1 == -1) { // 如果一方无效一方有效，取有效值
+                    // 如果是第二种情况，记录 +1，代表又用了一枚硬币
                     dp[i][j] = p2 + 1;
                 } else if (p2 == -1) {
                     dp[i][j] = p1;
-                } else {
+                } else { // 如果两者都有效，取用硬币数较少者
                     dp[i][j] = Math.min(p1, p2 + 1);
                 }
             }
         }
+        // 从第0枚硬币开始考虑，组成total价值，所以这个位置的记录就是答案
         return dp[0][total];
     }
 
+    /**
+     * 动态规划解决一维数组到达目标位置的问题
+     * 给一个数组范围，目标位置，步数，当前位置
+     * 每次必须向左或向右走一步，走到目标位置有多少种走法
+     * 
+     * @param N     数组从 0 到 N 位置
+     * @param E     目标位置
+     * @param steps 规定步数
+     * @param curr  当前位置
+     * @return
+     */
     public static int wayWalkDP(int N, int E, int steps, int curr) {
+        // 动态规划数组
+        // 从 0 到 step 步，所以为 step + 1
+        // 从 0 到 N 位置， 所以为 N+1
         int[][] dp = new int[steps + 1][N + 1];
+        // 初始化数组
         for (int i = 0; i <= steps; i++) {
             for (int j = 0; j <= N; j++) {
+
                 dp[i][j] = -1;
                 if (i == 0) {
+                    // 如果剩余步数为0，并且已在目标位置，那么走法为1
+                    // 否则为0
                     if (j == E) {
                         dp[i][j] = 1;
-                    } else if (j != 0) {
+                    } else if (j != 0) { // 0 位置越界
                         dp[i][j] = 0;
                     }
                 }
             }
         }
 
+        // 依次填写
         for (int i = 1; i <= steps; i++) {
             for (int j = 1; j <= N; j++) {
-                if (j == 1) {
+                if (j == 1) { // 如果走到最左边，只能往右走，走到2位置
                     dp[i][j] = dp[i - 1][2] == -1 ? -1 : dp[i - 1][2];
-                } else if (j == N) {
+                } else if (j == N) { // 如果走到最右边，只能往左走，走到 N - 1 位置
                     dp[i][j] = dp[i - 1][N - 1] == -1 ? -1 : dp[i - 1][N - 1];
                 } else {
+                    // 如果在中间
+                    // 如果向左或向右走无效，则当前位置也无效
                     if (dp[i - 1][j - 1] == -1 || dp[i - 1][j + 1] == -1) {
                         dp[i][j] = -1;
-                    } else {
+                    } else { // 否则向左向右两种结果相加
                         dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
                     }
                 }
             }
         }
+        // 从curr位置走step步，所以这个位置的记录就是答案
         return dp[steps][curr];
     }
 
--- src/Deque/Deque.java
+++ src/Deque/Deque.java
@@ -12,12 +12,17 @@ public class Deque {
         }
     }
 
+    /**
+     * 用array实现双端队列，头尾都可以增删元素
+     * 用两个指针分别指向头元素 与 尾元素之后一位
+     * 
+     */
     public static class ArrayDeque<V> {
-        private static final int EXPAND_BASE = 2;
+        private static final int EXPAND_BASE = 2; // 用于扩容
         private Object[] elements;
-        private int head;
-        private int tail;
-        private int size;
+        private int head; // 头元素位置
+        private int tail; // 尾元素后一位位置
+        private int size; // 存放了多少个有效元素
 
         public ArrayDeque(int length) {
             elements = new Object[length];
@@ -26,22 +31,34 @@ public class Deque {
             size = 0;
         }
 
+        /**
+         * 扩容
+         */
         private void expand() {
             Object[] newElements = new Object[elements.length * EXPAND_BASE];
 
+            // 从头元素开始，一直到array末端，将元素一一拷贝
             for (int i = head, j = 0; i < elements.length; i++, j++) {
                 newElements[j] = elements[i];
             }
 
+            // 从0位置开始，将剩下的元素一一拷贝
             for (int i = 0, j = elements.length - head; i < head; i++, j++) {
                 newElements[j] = elements[i];
             }
-
+            // 更新头尾元素位置
             head = 0;
             tail = elements.length;
             elements = newElements;
         }
 
+        /**
+         * 判断越界，并返回正确指针位置
+         * 比如：头部指针本来在0位置，从头部加入一个元素，头部指针左移，变成-1，越界，将头部指针放到array最末端
+         * 
+         * @param index
+         * @return
+         */
         private int getMod(int index) {
             if (index < 0) {
                 index += elements.length;
@@ -51,46 +68,78 @@ public class Deque {
             return index;
         }
 
+        /**
+         * 从头部加入一个元素
+         * 
+         * @param newEle
+         */
         public void addHead(Element<V> newEle) {
+            // 移动头部指针位置，并放入新元素
             head = getMod(head - 1);
             elements[head] = newEle;
-            if (head == tail) {
+            if (head == tail) { // 如果头部指针与尾部后一个位置指针重合，代表array已满，需要扩容
                 expand();
             }
             size++;
         }
 
+        /**
+         * 从尾部加入一个元素
+         * 
+         * @param newEle
+         */
         public void addTail(Element<V> newEle) {
+            // 移动尾部指针位置，并放入新元素
             elements[tail] = newEle;
             tail = getMod(tail + 1);
-            if (head == tail) {
+            if (head == tail) { // 如果头部指针与尾部后一个位置指针重合，代表array已满，需要扩容
                 expand();
             }
             size++;
         }
 
+        /**
+         * 从头部删除一个元素
+         * 
+         * @return
+         */
         @SuppressWarnings("unchecked")
         public Element<V> removeHead() {
             if (size == 0) {
                 return null;
             }
+            // 记录要删除的元素
             Element<V> remove = (Element<V>) elements[head];
+            // 更新头部位置。
+            // 注意：该元素其实仍被记录在array中，只是不在头部指针 - 尾部指针的范围中
             head = getMod(head + 1);
             size--;
             return remove;
         }
 
+        /**
+         * 从尾部删除一个元素
+         * 
+         * @return
+         */
         @SuppressWarnings("unchecked")
         public Element<V> removeTail() {
             if (size == 0) {
                 return null;
             }
+            // 更新尾部位置。
+            // 注意：该元素其实仍被记录在array中，只是不在头部指针 - 尾部指针的范围中
             tail = getMod(tail - 1);
-            Element<V> remove = (Element<V>) elements[tail];
+            Element<V> remove = (Element<V>) elements[tail]; // 记录要删除的元素
             size--;
             return remove;
         }
 
+        /**
+         * 查看头部元素
+         * 
+         * @return
+         */
         @SuppressWarnings("unchecked")
         public Element<V> peakHead() {
             if (size == 0) {
@@ -99,6 +148,11 @@ public class Deque {
             return (Element<V>) elements[head];
         }
 
+        /**
+         * 查看尾部元素
+         * 
+         * @return
+         */
         @SuppressWarnings("unchecked")
         public Element<V> peakTail() {
             if (size == 0) {
@@ -107,17 +161,29 @@ public class Deque {
             int lastIndex = getMod(tail - 1);
             return (Element<V>) elements[lastIndex];
         }
-
-        public int getSize() {
-            return size;
-        }
     }
 
+    /**
+     * 窗口问题
+     * 给一个数组与一个窗口大小，窗口从数组头部移动到尾部
+     * 记录每个窗口范围里最大的数字，并返回这个数组
+     * 如：给一个数组[ 4,3,5,3,6,1,7] 窗口为 4
+     * 窗口从[ 4,3,5 ] 一直移动到[ 6,1,7 ]
+     * 每次窗口中最大数字组成的数组为： [ 4,5,6,6,7 ]
+     * 
+     * @param <V>
+     * @param arr
+     * @param w   // 窗口大小
+     * @return
+     */
     public static <V> int[] largestArr(int[] arr, int w) {
+        // 无效参数处理
         if (arr == null || arr.length == 0 || w < 1 || w > arr.length) {
             return null;
         }
+        // 总共有 length - w + 1 个窗口位置
         int[] res = new int[arr.length - w + 1];
+        // 用来记录元素所在位置对应关系的两个hashmap
         HashMap<Element<Integer>, Integer> eleIndex = new HashMap<Element<Integer>, Integer>();
         HashMap<Integer, Element<Integer>> indexEle = new HashMap<Integer, Element<Integer>>();
         for (int i = 0; i < arr.length; i++) {
@@ -126,22 +192,32 @@ public class Deque {
             indexEle.put(i, (Element<Integer>) ele);
         }
 
+        // 利用双端队列
         ArrayDeque<Integer> deque = new ArrayDeque<Integer>(w);
         for (int i = 0; i < arr.length; i++) {
+            // 每次移动窗口，必然从尾部加入一个元素
+            // 初始形成w大小的窗口时，也需要加入一个元素
+            // 行程单调递减的双端队列，当队列不为空时，查看尾部元素，如果比新元素小，则移出这个元素
+            // 直到队列为空，或者尾部元素比新元素大，再放入新元素
             while (deque.peakTail() != null && deque.peakTail().value <= arr[i]) {
                 deque.removeTail();
             }
-
+            // 放入新元素
             deque.addTail((Element<Integer>) indexEle.get(i));
 
+            // 窗口仍未形成的时候，窗口内元素不够w大小，不作处理
+            // 窗口形成并开始移动时，在进行操作
             if (i + 1 >= w) {
+                // 如果队列不为空，查看头部元素，检查这个元素是否已经过期（即不在窗口范围内）
+                // 如果已经过期，删除这个元素
                 while (deque.peakHead() != null && eleIndex.get(deque.peakHead()) < (i - w + 1)) {
                     deque.removeHead();
                 }
+                // 因为单调递减，所以未过期的最大元素就是窗口范围内的最大元素
+                // 将这个元素记录在ers中
                 res[i + 1 - w] = deque.peakHead().value;
             }
         }
-
         return res;
     }
 
